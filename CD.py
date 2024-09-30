b53ebda316eed6235a42369d8ad5ed3aace123b6
Joshua Ganschow <ganschowjosh@gmail.com>
2024-09-30 18:06:57 -0500

    New solution

---
+++ CD.py
@@ -0,0 +1,15 @@
+while True:
+    n, m = map(int, input().split())
+    
+    if n == 0 and m == 0:
+        break
+    
+    jack = set(int(input()) for _ in range(n))
+    
+    count = 0
+    for _ in range(m):
+        cd = int(input())
+        if cd in jack:
+            count += 1
+    
+    print(count)
