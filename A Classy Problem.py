5752deee6e40c04e2554cc26b0adbd8b993c44f8
Joshua Ganschow <ganschowjosh@gmail.com>
2024-10-02 13:48:31 -0500

    New solutions

--- A Classy Problem.py
+++ A Classy Problem.py
@@ -1,30 +1,52 @@
-classDict = {"upper": 3, "middle": 2, "lower": 1}
+from collections import defaultdict
+from functools import cmp_to_key
 
-numCases = int(input())
-for _ in range(numCases):
-    numinCase = int(input())
-    peopleClasses = []
-    nameDict = {}
-    scoreDict = {}
-    nameLookup = {}
-    maxLen = 0
-    inputData = []
-    for i in range(numinCase):
-        inputData.append(input().split())
-    for i in range(numinCase):
-        name, classes, x = inputData[i]
-        name = name[:-1]
-        scoreDict[name] = 0
-        nameLookup[i] = name
-        cl = classes.split("-")
-        maxLen = max(maxLen, len(cl))
-        peopleClasses.append(cl)
-    for i in range(len(peopleClasses)):
-        while len(peopleClasses[i]) < maxLen:
-            peopleClasses[i].append("middle")
-        for j in range(len(peopleClasses[i])-1, -1, -1):
-            scoreDict[nameLookup[i]] += classDict[peopleClasses[i][j]]*(10**(len(peopleClasses[i])-j-1))
-    scoreList = sorted([(v, k) for k, v in scoreDict.items()], reverse=True)
-    for value, name in scoreList:
+#storing name and class value
+m = defaultdict(int)
+v = []
+
+#sorts vector, primarily by value, secondarily by name
+def sortVec(a, b):
+    al = m[a]
+    bl = m[b]
+    if al != bl:
+        return m[b] - m[a] #sort by class value (descending)
+    else:
+        return (a > b) - (a < b) #sort by name (ascending)
+
+
+t = int(input())
+for _ in range(t):
+    v.clear()
+    m.clear()
+    n = int(input())
+
+    #reading input
+    for _ in range(n):
+        inpString = input().split()
+        name = inpString[0][:-1]
+        classString = str(inpString[1])
+        
+
+        #class val calculation
+        classVal = ''
+        for token in classString.split('-'):
+            if token == "upper":
+                classVal += '3'
+            elif token == "middle":
+                classVal += '2'
+            elif token == "lower":
+                classVal += '1'
+        
+        classVal = classVal[::-1]
+        classVal = classVal.ljust(10, '2')
+
+        value = int(classVal)
+        m[name] = value
+        v.append(name)
+
+    v.sort(key=cmp_to_key(sortVec))
+
+    for name in v:
         print(name)
-    print("=" * 30)
+    print("="*30)
