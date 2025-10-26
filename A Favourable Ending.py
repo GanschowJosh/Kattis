f10037e1e841e189e31529c127c65f2fba1a4765
GanschowJosh <ganschowjosh@gmail.com>
2025-10-26 10:53:48 -0500

    fixing formatting

--- A Favourable Ending.py
+++ A Favourable Ending.py
@@ -5,28 +5,28 @@ t = int(input())
 
 
 for _ in range(t):
-ending_map = defaultdict(str)
-connection_map = defaultdict(list)
-s = int(input())
-for __ in range(s):
-  line = input().split()
-  ending = False
-  try:
-  line[1] = int(line[1])
-  except:
-  ending = True
-  if not ending:
-  curr, a, b, c = map(int, line)
-  connection_map[curr].extend([a, b, c])
-  else:
-  curr, ending = line
-  curr = int(curr)
-  ending_map[curr] = ending
+  ending_map = defaultdict(str)
+  connection_map = defaultdict(list)
+  s = int(input())
+  for __ in range(s):
+    line = input().split()
+    ending = False
+    try:
+      line[1] = int(line[1])
+    except:
+      ending = True
+    if not ending:
+      curr, a, b, c = map(int, line)
+      connection_map[curr].extend([a, b, c])
+    else:
+      curr, ending = line
+      curr = int(curr)
+      ending_map[curr] = ending
 
 @lru_cache(maxsize=None)
 def dfs(curr):
   if ending_map[curr] == "favourably":
-  return 1
+    return 1
   return sum(dfs(neighbor) for neighbor in connection_map[curr])
 
 print(dfs(1))
