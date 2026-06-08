8941e2e829ca84944d1f4974f2f3be2d0f13c639
Joshua Ganschow <ganschowjosh@gmail.com>
2026-06-08 10:59:32 -0500

    Rename Travel the Skies.py to traveltheskies.py

--- traveltheskies.py
+++ traveltheskies.py
@@ -1,20 +1,15 @@
 from collections import defaultdict
 def isoptimal(k, n, m, flights, customers):
-  #create a graph to represent flights
   graph = defaultdict(lambda: defaultdict(list))
   for u, v, d, z in flights:
     graph[d][u].append((v, z))
 
-  #create a dict to store customers at each airport for each day
   airportcustomers = defaultdict(lambda: defaultdict(int))
   for a, b, c in customers:
     airportcustomers[b][a] += c
   
-  #simulating flights for each day
   for day in range(1, n+1):
-    #for each airport, try to fill flights
     for airport in range(1, k+1):
-      #sorting flights by capacity to prioritize filling larger flights
       for dest, capacity in sorted(graph[day][airport], key=lambda x: -x[1]):
         passengers = min(airportcustomers[day][airport], capacity)
         if passengers < capacity:
@@ -23,7 +18,6 @@ def isoptimal(k, n, m, flights, customers):
         airportcustomers[day][airport] -= passengers
         airportcustomers[day+1][dest] += passengers
   
-    #move remaining customers to next day
     for airport in range(1, k+1):
       airportcustomers[day+1][airport] += airportcustomers[day][airport]
     
