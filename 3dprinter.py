423a12d11ea83b70807c5404fac5053c2406a286
Joshua Ganschow <ganschowjosh@gmail.com>
2026-06-08 09:42:33 -0500

    Rename 3D Printed Statues.py to 3dprinter.py

--- 3dprinter.py
+++ 3dprinter.py
@@ -1,23 +1,18 @@
 from collections import deque
 
 def min_days(n):
-  # Initial state is (number of printers, number of statues, number of days)
   initial_state = (1, 0, 0)
 
-  # Create a queue and add the initial state
   queue = deque([initial_state])
 
-  # Create a set to store visited states
   visited = set()
 
   while queue:
     printers, statues, days = queue.popleft()
 
-    # If we have printed enough statues, return the number of days
     if statues >= n:
       return days
 
-    # Generate new states and add them to the queue
     new_states = [(printers, statues + printers, days + 1), (printers * 2, statues, days + 1)]
 
     for new_state in new_states:
