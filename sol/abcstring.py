967d5b4c6f0adbc9388f51a02eb80b9223e1164d
GanschowJosh <ganschowjosh@gmail.com>
2026-07-02 15:43:53 -0500

    finally solving `abcstring`

--- sol/abcstring.py
+++ sol/abcstring.py
@@ -1,20 +1,10 @@
 s = input().strip()
-n = len(s)
 
-counts = {'A': 0, 'B': 0, 'C': 0}
-current = {'A': 0, 'B': 0, 'C': 0}
-result = 0
+cnt={'A':0,'B':0,'C':0}
 
-for char in s:
-  counts[char] -= 1
-  current[char] += 1
+ans=0
+for ch in s:
+  cnt[ch]+=1
+  ans=max(ans, max(cnt.values())-min(cnt.values()))
 
-  while all(current[c] > 0 for c in 'ABC'):
-    for c in 'ABC':
-      current[c] -= 1
-    result += 1
-  
-  if any(counts[c] < result for c in 'ABC'):
-    break
-
-print(result)
+print(ans)
